f61b42857e1a7045b4cc5ca176a37d4ccc0e5542
Dominik Holler <dholler@redhat.com>
2017-02-16 17:49:59 +0100

    Add QuantumListNetworksTimeout

---
+++ QuantumListNetworksTimeout/src/main/java/com/woorea/openstack/examples/network/QuantumListNetworksTimeout.java
@@ -0,0 +1,113 @@
+package com.woorea.openstack.examples.network;
+
+import com.woorea.openstack.connector.RESTEasyConnector;
+import com.woorea.openstack.keystone.model.Authentication;
+
+import com.woorea.openstack.base.client.OpenStackSimpleTokenProvider;
+import com.woorea.openstack.keystone.Keystone;
+import com.woorea.openstack.keystone.model.Access;
+import com.woorea.openstack.quantum.Quantum;
+import com.woorea.openstack.quantum.model.Network;
+import com.woorea.openstack.quantum.model.Networks;
+
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.params.HttpConnectionParams;
+import org.apache.http.params.HttpParams;
+import org.codehaus.jackson.annotate.JsonProperty;
+import org.codehaus.jackson.map.annotate.JsonRootName;
+import org.jboss.resteasy.client.ClientExecutor;
+import org.jboss.resteasy.client.core.executors.ApacheHttpClient4Executor;
+
+
+@JsonRootName(value = "auth")
+class ApiKey extends Authentication {
+	private ApiKey.ApiKeyCredentials apiKeyCredentials = new ApiKey.ApiKeyCredentials();
+
+	public ApiKey() {
+	}
+
+	public ApiKey(String username, String apiKey) {
+		this.apiKeyCredentials.setUsername(username);
+		this.apiKeyCredentials.setApiKey(apiKey);
+	}
+
+	@JsonProperty("RAX-KSKEY:apiKeyCredentials")
+	public ApiKey.ApiKeyCredentials getApiKeyCredentials() {
+		return this.apiKeyCredentials;
+	}
+
+	@JsonProperty("RAX-KSKEY:apiKeyCredentials")
+	public void setApiKeyCredentials(ApiKey.ApiKeyCredentials apiKeyCredentials) {
+		this.apiKeyCredentials = apiKeyCredentials;
+	}
+
+	public static final class ApiKeyCredentials {
+		private String username;
+		private String apiKey;
+
+		public ApiKeyCredentials() {
+		}
+
+		public String getUsername() {
+			return this.username;
+		}
+
+		public void setUsername(String username) {
+			this.username = username;
+		}
+
+		public String getApiKey() {
+			return this.apiKey;
+		}
+
+		public void setApiKey(String apiKey) {
+			this.apiKey = apiKey;
+		}
+	}
+}
+
+
+public class QuantumListNetworksTimeout {
+
+	static private class CustomizedRESTEasyConnector extends RESTEasyConnector {
+
+		private static final int tenSeconds = 10000;
+		private static final int connectionTimeout = tenSeconds;
+		private static final int socketTimeout = tenSeconds;
+
+		@Override
+		protected ClientExecutor createClientExecutor() {
+			DefaultHttpClient httpClient = new DefaultHttpClient();
+			HttpParams params = httpClient.getParams();
+			HttpConnectionParams.setConnectionTimeout(params, connectionTimeout);
+			HttpConnectionParams.setSoTimeout(params, socketTimeout);
+			return new ApacheHttpClient4Executor(httpClient);
+		}
+	}
+
+	private static final String KEYSTONE_AUTH_URL = "https://identity.api.rackspacecloud.com/v2.0";
+	private static final String KEYSTONE_USERNAME = "dominikholler";
+	private static final String KEYSTONE_PASSWORD = "";
+	private static final String ENDPOINT_URL = "https://lon.networks.api.rackspacecloud.com/v2.0";
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		Keystone keystone = new Keystone(KEYSTONE_AUTH_URL);
+
+		Access access = keystone.tokens().authenticate(
+				new ApiKey(KEYSTONE_USERNAME, KEYSTONE_PASSWORD))
+				.execute();
+
+		Quantum quantum = new Quantum(ENDPOINT_URL, new CustomizedRESTEasyConnector());
+		quantum.setTokenProvider(new OpenStackSimpleTokenProvider(access.getToken().getId()));
+
+		Networks networks = quantum.networks().list().execute();
+		for (Network network : networks) {
+			System.out.println(network);
+		}
+
+	}
+}
--- README.md
+++ README.md
@@ -1,1 +1,55 @@
 # openstack-java-sdk-verify
+
+## Get a new Fedora VM
+
+```
+rootpassword=123456
+mirror=https://download.fedoraproject.org/pub
+version=25
+name=Fedora-Cloud-Base-$version-1.3
+osvariant=fedora23
+origfile=$name.x86_64.qcow2
+image=$name.img
+
+wget -nc $mirror/fedora/linux/releases/$version/CloudImages/x86_64/images/$origfile
+
+truncate -s 40G $image
+virt-resize --expand /dev/sda1 $origfile $image
+virt-customize -a $image \
+    --root-password password:$rootpassword \
+    --ssh-inject root \
+    --selinux-relabel \
+    --uninstall cloud-init \
+    --hostname $name \
+    --timezone "Europe/Berlin" \
+    --update
+
+virt-install --name $name \
+    --vcpus 2,maxvcpus=4 \
+    --memory 1024,maxmemory=4096 \
+    --disk $image \
+    --os-type=linux --os-variant=$osvariant \
+    --import \
+    --connect qemu:///system &
+sleep 5
+virsh -c qemu:///system setmem  $name 1G
+arp | grep `virsh -c qemu:///system domiflist $name | tail -n 2 | sed -E 's/.*([0-9a-fA-F:]{17})/\1/'`
+
+```
+
+## Run Example Application using updates-testing repo
+```
+ssh root@92.168.122.204
+dnf config-manager --set-enabled updates-testing
+dnf install openstack-java-*-3.1.2 java-1.8.0-openjdk-devel maven git
+git clone git@github.com:dominikholler/openstack-java-sdk-verify.git
+cd openstack-java-sdk-verify/QuantumListNetworksTimeout
+# configure user credentials
+vi src/main/java/com/woorea/openstack/examples/network/QuantumListNetworksTimeout.java  +90
+mvn compile # compile using libs from maven
+# rm -rf ~/.m2/repository/ # remove libs from manve
+./test_on_fedora.sh # run app with the libs from fedora packages
+
+```
+
+
